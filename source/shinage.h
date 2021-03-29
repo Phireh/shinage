e45b19e70e5ea1fdbe0687a7855defd8cfed1481
David <Phireh@hotmail.com>
2021-03-29 22:05:03 +0200

    Fix macro typo

--- source/shinage.h
+++ source/shinage.h
@@ -38,10 +38,15 @@ int x11_window_width = 320;
 int x11_window_height = 200;
 unsigned int x11_window_border_size = 1;
 
+/* OpenGL globals */
+GLXContext glx_context;
+
 /* Convenience macros */
 // NOTE: ##__VA_ARGS__ is a compiler extension and may not be portable. Maybe check for compiler defs here.
-#define log_err(str, ...) fprintf(stderr, "[ERROR] (%s:%d: " str "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_err(str, ...) fprintf(stderr, "[ERROR] (%s:%d): " str "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define log_debug(str, ...) fprintf(stderr, "[DEBUG] (%s:%d: " str "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_debug(str, ...) fprintf(stderr, "[DEBUG] (%s:%d): " str "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+#define log_info(str, ...) fprintf(stderr, "[INFO] (%s:%d): " str "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 #endif
