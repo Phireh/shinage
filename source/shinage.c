c13d0c9abaa054c61109d31514666af766e47639
David <Phireh@hotmail.com>
2021-03-29 15:23:50 +0200

    Message loop and refactoring

--- source/shinage.c
+++ source/shinage.c
@@ -29,21 +29,66 @@ int main(int argc, char *argv[])
   x11_screen = DefaultScreenOfDisplay(x11_display);
   x11_screen_id = DefaultScreen(x11_display);
 
-  x11_window = XCreateSimpleWindow(x11_display, RootWindowOfScreen(x11_screen), 0, 0, 320, 200, 1, BlackPixel(x11_display, x11_screen_id), WhitePixel(x11_display, x11_screen_id));
+  int x11_window_border_color = BlackPixel(x11_display, x11_screen_id);
+  int x11_window_background_color = WhitePixel(x11_display, x11_screen_id);
+  // TODO: Check return status of this
+  x11_window = XCreateSimpleWindow(x11_display,
+                                   RootWindowOfScreen(x11_screen),
+                                   x11_window_x_coord,
+                                   x11_window_y_coord,
+                                   x11_window_width,
+                                   x11_window_height,
+                                   x11_window_border_size,
+                                   x11_window_border_color,
+                                   x11_window_background_color);
 
+  Atom wmDeleteMessage = XInternAtom(x11_display, "WM_DELETE_WINDOW", False);
+  XSetWMProtocols(x11_display, x11_window, &wmDeleteMessage, 1);
+                                   
+  XSelectInput(x11_display, x11_window, KeyPressMask | KeyReleaseMask | KeymapStateMask);
+
+  XStoreName(x11_display, x11_window, "Shinage");
+  
   XClearWindow(x11_display, x11_window);
   XMapRaised(x11_display, x11_window);
 
   /* Window message loop */
   XEvent x11_event;
-  while (1)
+  KeySym keysym = 0;
+  main_loop_state_t main_loop_state = RUNNING;
+  while (main_loop_state == RUNNING)
   {
-    XNextEvent(x11_display, &x11_event);
+      // TODO: Sleep so we don't burn the CPU
+      XNextEvent(x11_display, &x11_event);
+
+      switch (x11_event.type)
+      {
+      case KeymapNotify:
+          XRefreshKeyboardMapping(&x11_event.xmapping);
+          break;
+
+      case KeyPress:
+          keysym = XLookupKeysym(&x11_event.xkey, 0);
+          switch (keysym)
+          {
+          case XK_Escape:
+              main_loop_state = FINISHED;
+              break;
+          }
+          break;
+
+      case ClientMessage:
+          /* Petition to close window by the window manager. See Xlib's ICCCM docs */
+          if (x11_event.xclient.data.l[0] == (long int)wmDeleteMessage)
+              main_loop_state = FINISHED;
+          break;
+              
+          
+      }
   }
 
   /* Cleanup */  
   XDestroyWindow(x11_display, x11_window);
-  XFree(x11_screen);
   XCloseDisplay(x11_display);
   return 1;  
 }
--- source/shinage.h
+++ source/shinage.h
@@ -4,6 +4,7 @@
 /* CRT includes */
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 /* X11 server includes:
    requires linking with -lX11 */
@@ -11,6 +12,14 @@
 #include <X11/Xutil.h>
 
 /* OpenGL related includes */
+
+/* Types */
+typedef enum {
+    PAUSED,
+    RUNNING,
+    FINISHED,
+    TOTAL
+} main_loop_state_t;
 
 
 
@@ -21,8 +30,13 @@ Display *x11_display;
 Window x11_window;
 Screen *x11_screen;
 int x11_screen_id;
+int x11_window_x_coord = 0;
+int x11_window_y_coord = 0;
+int x11_window_width = 320;
+int x11_window_height = 200;
+unsigned int x11_window_border_size = 1;
 
 /* Convenience macros */
-#define log_err(str, ...) fprintf(stderr, "[ERROR] (%s:%d: %s" str "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_err(str, ...) fprintf(stderr, "[ERROR] (%s:%d: " str "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 #endif
